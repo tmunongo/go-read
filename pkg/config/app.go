bcd6f37b2d24d719b6c75f99fa2a10da2bb47ef4
Tawanda Munongo <ejmunongo@gmail.com>
2023-11-11 15:53:20 +0200

    final commit

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,8 @@
 package config
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var (
@@ -10,7 +10,8 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "root:password@tcp(localhost:3306)/go-read?charset=utf8&parseTime=True&loc=Local")
+	dsn := "dbuser:password@tcp(127.0.0.1:3306)/go-read?charset=utf8mb4&parseTime=True&loc=Local"
+  	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,8 +1,8 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/tmunongo/go-read/pkg/config"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
@@ -18,4 +18,28 @@ func init(){
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var theBook Book
+
+	db := db.Where("ID=?", Id).Find(&theBook)
+	return &theBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
 }
