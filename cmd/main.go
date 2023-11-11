ba17dc7de6395a4528b7b72ff99ebe774d84da74
Tawanda Munongo <ejmunongo@gmail.com>
2023-11-11 15:53:03 +0200

    final commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/tmunongo/go-read/pkg/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
